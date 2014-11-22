87596adc1b261adcd96f3174c844268ccb22a514
Peter Stephan <pstephan@peter-stephans-macbook-pro.local>
2014-11-22 13:59:26 -0500

    Fix log message

--- web_server.py
+++
@@ -1,7 +0,0 @@
-from twisted.internet import reactor
-from twisted.web import server, static
-
-resource = static.File('.')
-factory = server.Site(resource)
-reactor.listenTCP(3000, factory, interface='localhost')
-reactor.run()
